4892828839fea5ae91de86f93abac2e05faa13ce
justjo3l <comedyandcomputers@gmail.com>
2024-01-28 23:45:04 +0400

    Add test for guessName()

--- tests/goldle.test.js
+++ tests/goldle.test.js
@@ -230,3 +230,50 @@ describe('testing rigGame() in Goldle', () => {
         expect(goldle.rigGame("John Doe")).toStrictEqual(null);
     });
 });
+
+describe('testing guessName() in Goldle', () => {
+    let goldle;
+
+    beforeEach(() => {
+        goldle = new Goldle();
+        goldle.setupGators();
+    });
+
+    describe('errors', () => {
+        test('guessName() should return an error if game has not started', () => {
+            expect(goldle.guessName("Joel Jose").error).toStrictEqual("Game not started");
+        });
+
+        test('guessName() should return an error if name does not exist', () => {
+            goldle.startGame();
+            expect(goldle.guessName("John Doe").error).toStrictEqual("Gator not found");
+        });
+    });
+
+    describe('valid actions', () => {
+        beforeEach(() => {
+            goldle.startGame();
+            goldle.rigGame("Joel Jose");
+        });
+
+        test('guessName() should return a won gamestate if name is correct', () => {
+            const result = goldle.guessName("Joel Jose");
+            expect(result.guessState).not.toStrictEqual(undefined);
+            expect(result.gameState).toStrictEqual("won");
+        });
+
+        test('guessName() should return a guessState and lost gameState if name is incorrect and no guesses remain', () => {
+            goldle.numGuesses = 1;
+            const result = goldle.guessName("Amber Chan");
+            expect(result.guessState).not.toStrictEqual(undefined);
+            expect(result.gameState).toStrictEqual("lost");
+        });
+
+        test('guessName() should return a guessState if guess is made', () => {
+            const result = goldle.guessName("Amber Chan");
+            expect(result.guessState).not.toStrictEqual(undefined);
+            expect(result.gameState).toStrictEqual(undefined);
+        });
+    });
+
+});
