f94eee4cbf210b4371b6eebe42840d56c5d1d198
justjo3l <comedyandcomputers@gmail.com>
2024-01-28 22:46:37 +0400

    Add tests for searchName()

--- tests/goldle.test.js
+++ tests/goldle.test.js
@@ -10,11 +10,11 @@ describe('testing getGatorByName() in Goldle', () => {
     });
 
     test('getGatorByName() should return gator if name exists', () => {
-        expect(goldle.getGatorByName('Joel Jose')).not.toEqual(null);
+        expect(goldle.getGatorByName('Joel Jose')).not.toStrictEqual(null);
     });
 
     test('getGatorByName() should return null if name does not exist', () => {
-        expect(goldle.getGatorByName('John Doe')).toEqual(null);
+        expect(goldle.getGatorByName('John Doe')).toStrictEqual(null);
     });
 });
 
@@ -28,7 +28,7 @@ describe('testing getRandomGator() in Goldle', () => {
     });
 
     test('getRandomGator() should return a gator', () => {
-        expect(goldle.getRandomGator()).not.toEqual(null);
+        expect(goldle.getRandomGator()).not.toStrictEqual(null);
     });
 });
 
@@ -42,11 +42,29 @@ describe('testing getState() and startGame() in Goldle', () => {
     });
 
     test('getState() should not return inactive if game has not started', () => {
-        expect(goldle.getState()).toEqual('inactive');
+        expect(goldle.getState()).toStrictEqual('inactive');
     });
 
     test('getState() should return started if game has started', () => {
         goldle.startGame();
-        expect(goldle.getState()).toEqual('started');
+        expect(goldle.getState()).toStrictEqual('started');
+    });
+});
+
+describe('testing searchName() in Goldle', () => {
+    
+    let goldle;
+
+    beforeEach(() => {
+        goldle = new Goldle();
+        goldle.setupGators();
+    });
+
+    test('searchName() should return list of options if name could potentially exist', () => {
+        expect(goldle.searchName('joe').length).not.toStrictEqual(0);
+    });
+
+    test('searchName() should return an empty array if no name options exist', () => {
+        expect(goldle.searchName('John Doe').length).toStrictEqual(0);
     });
 });
