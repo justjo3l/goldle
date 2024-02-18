a47664136ca2d6d811c4ce1f926259e411afdaff
justjo3l <comedyandcomputers@gmail.com>
2024-02-18 19:53:53 +1100

    Test for addition and removal on on-error to guessbar

--- src/tests/guessBar.test.js
+++ src/tests/guessBar.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import GuessBar from 'components/GuessBar/GuessBar.js';
 
-import '@testing-library/jest-dom';
-import { describe, test, expect } from '@jest/globals';
+import "@testing-library/jest-dom";
+import { describe, jest, test, expect } from '@jest/globals';
 
 import { render, fireEvent } from '@testing-library/react';
 
@@ -20,14 +20,20 @@ describe('testing GuessBar', () => {
         expect(guessBar).toBeInTheDocument();
     });
 
-    test('GuessBar should not do anything if error encountered', () => {
+    test('GuessBar should have on-error class and not do anything if error encountered', () => {
         const goldle = new Goldle();
         goldle.startGame();
         goldle.rigGame('Joel Jose');
         render(<GuessBar goldle={goldle} onGuess={() => {}} onError={() => {}} />);
-        const guessBar = document.getElementById('guess-bar');
+        jest.useFakeTimers();
+        let guessBar = document.getElementById('guess-bar');
         fireEvent.change(guessBar, {target: {value: 'Jo'}});
         fireEvent.keyDown(guessBar, {key: 'Enter', code: 'Enter'});
         expect(guessBar).toBeInTheDocument();
+        expect(guessBar.classList).toContain('on-error');
+        jest.runAllTimers();
+        guessBar = document.getElementById('guess-bar');
+        expect(guessBar.classList).not.toContain('on-error');
+        jest.useRealTimers();
     });
 });
