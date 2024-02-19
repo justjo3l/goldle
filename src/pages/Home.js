cc7c0d8eb8135bed89d63c0bb642342448aa2dd3
justjo3l <comedyandcomputers@gmail.com>
2024-02-20 01:13:45 +1100

    Remove E2E testing rigging

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -106,7 +106,6 @@ export default function Home() {
   const handleStartClick = () => {
     resetGame();
     goldle.startGame();
-    goldle.rigGame("Blair Dowling");
     setMaxGuesses(goldle.numGuesses);
     setState(goldle.getState());
   };
